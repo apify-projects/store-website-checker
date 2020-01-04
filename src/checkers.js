ef7f2d32997c490b9d383ce9aef5cd1be7b53baf
metalwarrior665 <lukaskrivka@gmail.com>
2020-01-04 17:36:10 +0100

    added recatpcha

--- src/checkers.js
+++ src/checkers.js
@@ -7,9 +7,15 @@ const accessDenied = ($) => {
     return $('title').text().includes('Access Denied');
 };
 
+const recaptcha = ($) => {
+    return $('#recaptcha').length > 0
+        || $('iframe[src*="/recaptcha/"]').length > 0
+}
+
 module.exports.testHtml = ($) => {
     return {
         distilCaptcha: distilCaptcha($),
         accessDenied: accessDenied($),
+        recaptcha: recaptcha($),
     };
 }
